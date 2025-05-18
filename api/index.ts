ee3ff0c80d7eb5d1b4971092ff10ff6df06414f6
Chirkov <tchirkov.petter@gmail.com>
2025-05-18 04:09:44 +0300

    scaffold project

---
+++ api/index.ts
@@ -0,0 +1,13 @@
+import express, { Application } from 'express'
+import cors from 'cors'
+
+
+const app: Application = express()
+app.use(cors())
+
+app.get('/', (_, res) => {
+    res.send({ message: 'huy' })
+})
+
+
+app.listen(3000, () => console.log('server started'))
---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const huy = ref('')
+
+const fetchHuy = async () => {
+  const response = await fetch('/api')
+  const data = await response.json()
+  huy.value = data
+}
+
+onMounted(() => fetchHuy())
+</script>
+
+<template>
+  <h1>{{ huy }}</h1>
+</template>
